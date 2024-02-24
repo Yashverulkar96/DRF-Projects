4cf69ad4ec26597facdafbb0031f179c06666833
Yashverulkar96 <yverulkar02@gmail.com>
2024-02-24 19:00:20 +0530

    Project updated.

--- Virtual Env/Class_based_view/User/urls.py
+++ Virtual Env/Class_based_view/User/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path,include
 
 urlpatterns = [
-   path('',views.User.as_view()),
-   path('<int:pk>/',views.User_details.as_view()),
+   path('user/',views.User.as_view()),
+   path('user/<int:pk>/',views.User_details.as_view()),
 ]
--- Virtual Env/Simple_Crud/user/models.py
+++ Virtual Env/Simple_Crud/user/models.py
@@ -6,6 +6,19 @@ class User(models.Model):
     name = models.CharField(max_length=100,primary_key=True)
     phone_number= PhoneNumberField()
     is_admin= models.BooleanField()
+
+    # class Meta:
+    #    verbose_name_plural  = "Categories"
+        # ordering = ['-created']
     
     def __str__(self):
         return self.name
+    
+
+class Address(models.Model):
+    Address=models.TextField()
+    # address will automatically delete is user is deleted.
+    User=models.ForeignKey(User, on_delete=models.CASCADE, blank=True , null=True)
+
+    def __str__(self):
+        return f'{self.Address} |{self.User}'
